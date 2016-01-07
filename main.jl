3d7b44aaf77910f9536f9103821f8175ac38d411
Jake Rosoman <jkroso@gmail.com>
2016-01-07 15:38:02 +1300

    use BioJulia/BufferedStreams

--- main.jl
+++ main.jl
@@ -1,44 +1,49 @@
-const whitespace = " \t\n\r"
+@require "github.com/BioJulia/BufferedStreams.jl" peek BufferedInputStream
 
-function skipwhitespace(io::IO)
+const whitespace = UInt8[" \t\n\r"...]
+const digits = UInt8["0123456789+-"...]
+isdigit(n::UInt8) = n in digits
+
+function skipwhitespace(io::BufferedInputStream)
   while true
-    c = read(io, Char)
+    c = read(io, UInt8)
     c in whitespace || return c
   end
 end
 
 parse(json::AbstractString) = parse(IOBuffer(json))
+parse(json::IO) = parse(BufferedInputStream(json))
 
-function parse(io::IO)
+function parse(io::BufferedInputStream)
   c = skipwhitespace(io)
-  if     c ≡ '"' parse_string(io)
-  elseif c ≡ '{' parse_dict(io)
-  elseif c ≡ '[' parse_vec(io)
-  elseif isdigit(c) || c ≡ '+' || c ≡ '-' parse_number(c, io)
-  elseif c ≡ 't' && readbytes(io, 3) == ["rue"... ] true
-  elseif c ≡ 'f' && readbytes(io, 4) == ["alse"...] false
-  elseif c ≡ 'n' && readbytes(io, 3) == ["ull"... ] nothing
-  else error("Unexpected char: $c") end
+  if     c == '"' parse_string(io)
+  elseif c == '{' parse_dict(io)
+  elseif c == '[' parse_vec(io)
+  elseif isdigit(c) || c == '+' || c == '-' parse_number(c, io)
+  elseif c == 't' && readbytes(io, 3) == ["rue"... ] true
+  elseif c == 'f' && readbytes(io, 4) == ["alse"...] false
+  elseif c == 'n' && readbytes(io, 3) == ["ull"... ] nothing
+  else error("Unexpected char: $(convert(Char, c))") end
 end
 
 test("primitives") do
   @test !parse("false")
   @test parse("true")
-  @test parse("null") ≡ nothing
+  @test parse("null") == nothing
 end
 
-function parse_number(c::Char, io::IO)
+function parse_number(c::UInt8, io::BufferedInputStream)
   Type = Int32
   buf = UInt8[c]
   while !eof(io)
-    c = read(io, Char)
-    if c ≡ '.'
-      @assert Type ≡ Int32 "malformed number"
+    c = peek(io)
+    if c == '.'
+      @assert Type == Int32 "malformed number"
       Type = Float32
     elseif !isdigit(c)
-      skip(io, -1)
       break
     end
+    read(io, UInt8)
     push!(buf, c)
   end
   Base.parse(Type, ascii(buf))
@@ -51,23 +56,23 @@ test("numbers") do
   @test parse("1.0") == 1.0
 end
 
-function parse_string(io::IO)
+function parse_string(io::BufferedInputStream)
   buf = IOBuffer()
   while true
-    c = read(io, Char)
-    c ≡ '"' && return takebuf_string(buf)
-    if c ≡ '\\'
-      c = read(io, Char)
-      if c ≡ 'u' write(buf, unescape_string("\\u$(utf8(readbytes(io, 4)))")[1]) # Unicode escape
-      elseif c ≡ '"'  write(buf, '"' )
-      elseif c ≡ '\\' write(buf, '\\')
-      elseif c ≡ '/'  write(buf, '/' )
-      elseif c ≡ 'b'  write(buf, '\b')
-      elseif c ≡ 'f'  write(buf, '\f')
-      elseif c ≡ 'n'  write(buf, '\n')
-      elseif c ≡ 'r'  write(buf, '\r')
-      elseif c ≡ 't'  write(buf, '\t')
-      else error("Unrecognized escaped character: $c") end
+    c = read(io, UInt8)
+    c == '"' && return takebuf_string(buf)
+    if c == '\\'
+      c = read(io, UInt8)
+      if c == 'u' write(buf, unescape_string("\\u$(utf8(readbytes(io, 4)))")[1]) # Unicode escape
+      elseif c == '"'  write(buf, '"' )
+      elseif c == '\\' write(buf, '\\')
+      elseif c == '/'  write(buf, '/' )
+      elseif c == 'b'  write(buf, '\b')
+      elseif c == 'f'  write(buf, '\f')
+      elseif c == 'n'  write(buf, '\n')
+      elseif c == 'r'  write(buf, '\r')
+      elseif c == 't'  write(buf, '\t')
+      else error("Unrecognized escaped character: $(convert(Char, c))") end
     else
       write(buf, c)
     end
@@ -80,17 +85,17 @@ test("strings") do
   @test parse("\"\\u0026\"") == "&"
 end
 
-function parse_vec(io::IO)
-  vec = []
-  skipwhitespace(io) ≡ ']' && return vec
-  skip(io, -1)
+function parse_vec(io::BufferedInputStream)
+  vec = Any[]
   while true
+    c = peek(io)
+    c == ']' && (read(io, UInt8); return vec)
+    c in whitespace && (read(io, UInt8); continue)
     push!(vec, parse(io))
     c = skipwhitespace(io)
-    c ≡ ']' && break
-    @assert c ≡ ',' "missing comma"
+    c == ']' && return vec
+    @assert c == ',' "missing comma"
   end
-  return vec
 end
 
 test("Vector") do
@@ -100,20 +105,19 @@ test("Vector") do
   @test parse("[ 1, 2 ]") == Any[1,2]
 end
 
-function parse_dict(io::IO)
+function parse_dict(io::BufferedInputStream)
   dict = Dict{AbstractString,Any}()
-  skipwhitespace(io) ≡ '}' && return dict
-  skip(io, -1)
   while true
-    key = parse(io)
-    @assert isa(key, AbstractString) "dictionary keys must be strings"
-    @assert skipwhitespace(io) ≡ ':' "missing semi-colon"
+    c = skipwhitespace(io)
+    c == '}' && return dict
+    @assert c == '"' "dictionary keys must be strings"
+    key = parse_string(io)
+    @assert skipwhitespace(io) == ':' "missing semi-colon"
     dict[key] = parse(io)
     c = skipwhitespace(io)
-    c ≡ '}' && break
-    @assert c ≡ ',' "missing comma"
+    c == '}' && return dict
+    @assert c == ',' "missing comma"
   end
-  return dict
 end
 
 test("Dict") do
@@ -121,4 +125,4 @@ test("Dict") do
   @test open(parse, "Readme.ipynb")["metadata"]["language_info"]["name"] == "julia"
 end
 
-Base.parse(::MIME"application/json", io::IO) = parse(readall(io))
+Base.parse(::MIME"application/json", io::IO) = parse(io)
